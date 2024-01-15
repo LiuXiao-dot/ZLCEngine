0ce830e1c48a57e802ce4f526c523709fdd31a9b
LiuXiao-dot <54983593+LiuXiao-dot@users.noreply.github.com>
2024-01-15 21:28:48 +0800

    暂停布尔运算开发
    1.因为希望能够保存完整路径，并且细致的编辑，布尔运算不支持

---
+++ Scripts/ZLCEditor/TerrainSystem/Boolean/TerrainEditorStateMachine/BooleanTerrainContext.cs
@@ -0,0 +1,10 @@
+namespace ZLCEditor.TerrainSystem.Boolean.TerrainEditorStateMachine
+{
+    /// <summary>
+    /// 地形上下文
+    /// </summary>
+    public class BooleanTerrainContext
+    {
+        
+    }
+}
---
+++ Scripts/ZLCEditor/TerrainSystem/Boolean/TerrainEditorStateMachine/SelectState.cs
@@ -0,0 +1,16 @@
+using ZLCEngine.NodeSystem.StateMachine;
+using ZLCEngine.NodeSystem.StateMachine.Basic;
+namespace ZLCEditor.TerrainSystem.Boolean.TerrainEditorStateMachine
+{
+    /// <summary>
+    /// 选择形状状态
+    /// </summary>
+    public class SelectState : AState
+    {
+
+        public override void Update(IContext context)
+        {
+            
+        }
+    }
+}
